67cbc7a5046e816c049c93c580a0d5c8501482b3
Shang <vuvu0130@gmail.com>
2021-05-19 15:38:50 +0800

    sOS

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 //dom 
 
-const app =document.querySelector('#app');
-const userName =document.querySelector('#username');
-const password =document.querySelector('#password');
+const viewBoard =document.querySelector('#app');
+
 const submit =document.querySelector('#submit');
 //api
 const url ='https://vue3-course-api.hexschool.io/';
@@ -11,31 +10,97 @@ const path ="shang";
 
 //data
 
-console.clear();
- submit.addEventListener("click",(e)=>{ 
-   e.preventDefault();
+
+ submit.addEventListener("click",login);
+
+ function login(){
+   const username =document.querySelector('#username').value;
+   const password =document.querySelector('#password').value;
    let user ={
-     username:userName.value,
-     password:password.value
-}  ;      
+     username,
+     password
+    }  ;     
      //post發出登入請求
     axios.post(`${url}admin/signin`,user).then((res)=>{
-     //儲存token與expire
-      let token =res.data.token;
-      let expired = res.data.expired;      
-      document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;    
-      //取得cookie內的token
-      render();
-   })
+         if (res.data.success) {
+            const { token, expired } = res.data;            
+            document.cookie = `hexToken=${token};expires=${new Date(expired)}`;
+            //初始化後台頁面
+             app.init();
+          }
+          //帳號錯誤
+          else{
+            alert("你的帳號錯誤窩窩")
+          }
+    
+      
+    
+   }) 
+ 
+ }
+const app = {
+  data:{
+     products: [],
+  },
+  getProducts(){
+    axios.get(`${url}api/${path}/admin/products`).then(res=>{
+     
+      //把取得的data放進products
+     this.products = res.data.products;
+     this.render();
+  
+        })
 
-   
- });
+  },
+   //新增
+ addNewProduct(){
+    
+      //預設新增產品資料
+        const product = {
+          data: {
+            title: "[賣]動物園造型衣服5",
+            category: "衣服2",
+            origin_price: 100,
+            price: 300,
+            unit: "個",
+            description: "Sit down please 名設計師設計",
+            content: "這是內容",
+            is_enabled: 0,
+            imageUrl:
+              "https://images.unsplash.com/photo-1573662012516-5cb4399006e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+          },
+        };
 
- function render(){
-   const tableline = document.querySelector("#productList") ;  
-     //如果tableline未渲染
-       if(!tableline){
-            //重新渲染
+        // #6 新增一個產品
+       axios.post(`${url}api/${path}/admin/product`,product).then(res=>{
+       app.getProducts();
+       })
+
+
+ },
+  deleteProduct(e){
+   let deleteBtn =e.target;
+ 
+  //鎖定deletebtn
+   if( deleteBtn.getAttribute("class").includes("deleteBtn") ){
+     let productId =deleteBtn.dataset.id;
+     //發出delete請求
+     axios.delete(`${url}api/${path}/admin/product/${productId}`).then(res=>{
+      
+      app.getProducts();
+
+      
+     })
+   }
+ 
+ },
+  init(){   
+    
+     //取得cookie內的token
+
+     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
+      if(token){
+             //重新渲染成後台產品檢視模式
         let str =` <table class="table mt-4">
            <thead>
              <tr>
@@ -60,24 +125,28 @@ console.clear();
          </table>
          <p>目前有 <span id="productCount">0</span> 項產品</p>               
           <button id="addNew">新增產品</button>`;
-         app.innerHTML =str;
-
-       }
-
+         viewBoard.innerHTML =str;
+         //監聽新增事件
+        addNew.addEventListener('click',this.addNewProduct);
+     
+     //取得產品列表
+       //將token夾帶進axios的header中
+        axios.defaults.headers.common.Authorization = token;
+      this.getProducts()
+      }
       
-   //取得cookie內的token，設定到header中
-      token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
-      axios.defaults.headers.common.Authorization = token;
-      //發出請求，取得商品資料
-        axios.get(`${url}api/${path}/admin/products`).then(res=>{
-          let products = res.data.products;
-          
-      let str2='';     
+   
+      
+      
+    
+  },
+  render(){
+        let str2='';     
      const tableline = document.querySelector("#productList") ;
      const productCount=document.querySelector("#productCount") ;
      
      //組資料中的各個商品列html    
-    products.forEach(item => {
+    this.products.forEach(item => {
         str2 += ` <tr>
               <td>${item.title}</td>
               <td width="120">
@@ -95,62 +164,19 @@ console.clear();
             </tr>`
       });
       //統計總商品數字
-      productCount.textContent =products.length;
+      productCount.textContent =this.products.length;
       tableline.innerHTML =str2;
-      //綁定render的dom
-       const addNew = document.querySelector("#addNew") ;
-        
-      
-       //監聽剛render的dom
-        addNew.addEventListener('click',addNewProduct)
-        tableline.addEventListener('click',deleteProduct)
-        })
-
-    
- } 
-
- //新增
- function addNewProduct(){
-    //取得cookie內的token
-    let  token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
-    axios.defaults.headers.common.Authorization = token;
-      //預設新增產品資料
-        const product = {
-          data: {
-            title: "[賣]動物園造型衣服4",
-            category: "衣服2",
-            origin_price: 100,
-            price: 300,
-            unit: "個",
-            description: "Sit down please 名設計師設計",
-            content: "這是內容",
-            is_enabled: 0,
-            imageUrl:
-              "https://images.unsplash.com/photo-1573662012516-5cb4399006e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-          },
-        };
-
-        // #6 新增一個產品
-       axios.post(`${url}api/${path}/admin/product`,product).then(res=>{
-         //重新渲染新的產品列
-         render()
-       })
+     
+      //監聽delete事件
+        tableline.addEventListener('click',this.deleteProduct)
+  }
+};
 
 
- }
+
+//驗證是否已是登入狀況
+app.init()
+
 
  //刪除
- function deleteProduct(e){
-   let deleteBtn =e.target;
- 
-  //鎖定deletebtn
-   if( deleteBtn.getAttribute("class").includes("deleteBtn") ){
-     let productId =deleteBtn.dataset.id;
-     //發出delete請求
-     axios.delete(`${url}api/${path}/admin/product/${productId}`).then(res=>{
-       //重新渲染結果
-       render();
-     })
-   }
- 
- }
+
